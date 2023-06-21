ffc0d30b237c377b0e0c0f290ac626306e75188c
pradeep singh <pradeepsi2120032@gmail.com>
2023-06-21 10:13:41 +0530

    added minor changes

--- client/src/components/quoteforms/forms.jsx
+++ client/src/components/quoteforms/forms.jsx
@@ -93,7 +93,7 @@ const   Forms = () => {
           <form onSubmit={submitform} className={selectedOption === 'commercial' ? 'visible' : 'hidden'}>
   <h2>Commercial Moving quote</h2>
   <input type="text" name="fullName" placeholder="Full Name"  />
-  <input type="email" name="email" placeholder="Email"  />
+  <input  name="email" placeholder="Email"  />
   <input type="text" name="from" placeholder="From"  />
   <input type="text" name="to" placeholder="To"  />
   <input type="date" name="date" placeholder="Date"  />
